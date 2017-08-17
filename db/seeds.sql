7d4158004b1885ff341d3f7fcb2c1fab69058a1d
Caleb Sandowich <calebsandowich@Calebs-MacBook-Pro.local>
2017-08-16 21:22:24 -0700

    added passwords to database and wireframe to readme

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,3 @@
-INSERT INTO host (host_name, host_phone, address, address, email) VALUES ('Jimmy Butterboy',0123456789, '2222 Packer Way, Los Angeles, CA 94855', 'tgames@email.com');
+INSERT INTO host (host_name,host_pass, host_phone, address, email) VALUES ('Jimmy Butterboy', 'password11', 0123456789, '2222 Packer Way, Los Angeles, CA 94855', 'tgames@email.com');
 INSERT INTO rating (likes, dislikes) VALUES (0 ,0);
-INSERT INTO user (user_name, email) VALUES ('gamer42', 'gamer42@email.com');
+INSERT INTO user (user_name, user_pass, email) VALUES ('gamer42', 'password12', 'gamer42@email.com');
