0e268c39bcca3cfc9459c052a6037f6870853eb3
Kevin Tran <kevin91801@yahoo.com>
2017-08-19 14:01:57 -0700

    updated models, installed passport node package

--- models/game.js
+++ models/game.js
@@ -1,3 +1,5 @@
+var bcrypt = require("bcrypt");
+
 module.exports = function(sequelize, DataTypes) {
 
   var host = sequelize.define("host", {
@@ -90,6 +92,22 @@ module.exports = function(sequelize, DataTypes) {
     }
     
   });
+
+host.methods.generateHash = function(host_pass) {
+    return bcrypt.hashSync(host_pass, bcrypt.genSaltSync(9));
+  }
+
+host.methods.validPassword = function(host_pass) {
+  return bcrypt.compareSync(host_pass, this.local.host_pass);
+}
+
+user.methods.generateHash = function(user_pass) {
+    return bcrypt.hashSync(user_pass, bcrypt.genSaltSync(9));
+  }
+
+user.methods.validPassword = function(user_pass) {
+  return bcrypt.compareSync(user_pass, this.local.user_pass);
+}
 
   return host, user, rating;
 };
