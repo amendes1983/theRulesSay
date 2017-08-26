bd1d3acc3b029b033784d900260d1d1be23eb5ab
Caleb Sandowich <calebsandowich@Calebs-MacBook-Pro.local>
2017-08-26 09:16:09 -0700

    added proper linking

--- public/js/user.js
+++ public/js/user.js
@@ -1,23 +1,17 @@
-var db = require("../models/");
 
 
-// $(document).ready(function(){
-//
-// addData();
-//
-// $('.collapsible').collapsible();
-//     function addData(){
-//       var newUl = $('<ul class="collapsible" data-collapsible="accordion"><li><div class="collapsible-header"><i class="material-icons">filter_drama</i>First</div><div class="collapsible-body"><span>Lorem ipsum dolor sit amet.</span></div></li></ul>');
-//       console.log(newUl);
-//       $('#userData').append(newUl);
-//
-//
-//     }
-//
-// });
+
 $(document).ready(function(){
-  db.host.findAll({where: {id: host.id}}).success(function(host) {
-    done(null, host);
-  })
+
+addData();
+
+$('.collapsible').collapsible();
+    function addData(){
+      var newUl = $('<ul class="collapsible" data-collapsible="accordion"><li><div class="collapsible-header"><i class="material-icons">CalebBanana</i></div><div class="collapsible-body"><span>bananaboy@gmail.com</span></div></li></ul>');
+      console.log(newUl);
+      $('#userData').append(newUl);
+
+
+    }
 
 });
