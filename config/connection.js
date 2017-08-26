ddb3fd2faf6fa1ff0cb8947952a97afca607d06d
Kevin Tran <kevin91801@yahoo.com>
2017-08-26 10:34:41 -0700

    added jawsdb url to connection

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,19 @@
 var Sequelize = require("sequelize");
+var sequelize;
 
+if (process.env.JAWSDB_URL) {
+	sequelize = new Sequelize(process.env.JAWSDB_URL, {
+		dialect: "mysql",
+		pool: {
+			max: 5,
+			min: 0,
+			idle: 10000
+		}
+	})
+}
 
-var sequelize = new Sequelize("therulessay", "root", "", {
+else {
+	sequelize = new Sequelize("therulessay", "root", "", {
   host: "localhost",
   dialect: "mysql",
   pool: {
@@ -10,5 +22,5 @@ var sequelize = new Sequelize("therulessay", "root", "", {
     idle: 10000
   }
 });
-
+}
 module.exports = sequelize;
